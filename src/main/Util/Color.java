0883c7a31c5edb4f8832d9fb2e233d674bf17068
QuantumDeity <greasyRooster1@gmail.com>
2023-11-29 16:37:55 -0800

    Chapter 9.5

--- src/main/Util/Color.java
+++ src/main/Util/Color.java
@@ -2,12 +2,18 @@ package main.Util;
 
 import main.Main;
 
+import static java.lang.Math.sqrt;
+
 public class Color extends Vec3{
     public Color() {
         e = new double[]{0,0,0};
     }
     public Color(double e0, double e1, double e2){
         e = new double[]{e0,e1,e2};
+    }
+    public static double linearToGamma(double linearComponent)
+    {
+        return sqrt(linearComponent);
     }
     public static void write(int x,int y,Vec3 col, int samples_per_pixel){
         double r = col.x();
@@ -19,6 +25,9 @@ public class Color extends Vec3{
         r *= scale;
         g *= scale;
         b *= scale;
+        r=linearToGamma(r);
+        g = linearToGamma(g);
+        b = linearToGamma(b);
         Vec3 avgCol = new Vec3(r,g,b);
         Main.app.set(x,y,avgCol.rgb());
     }
